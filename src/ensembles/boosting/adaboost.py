Fix gradient calculation in linear models; adjust penalty term sign in TrainingStrategy and update verbosity level in fit methods across AdaBoost and GradientBoostedRegressionTree classes for consistency. Modify derivative methods in regression impurity measures to correct sign.

src/ensembles/boosting/adaboost.py
@@ -54,7 +54,7 @@ class AdaBoostClassifier:
             raise ValueError(f"Unsupported loss function: {loss}")
     
 
-    def fit(self, x_train: np.ndarray, y_train: np.ndarray, verbose: int = 2):
+    def fit(self, x_train: np.ndarray, y_train: np.ndarray, verbose: int = 1):
         if self.impurity_type is None:
             raise ValueError("The model is not compiled yet. Please call the compile method before fit.")
 
@@ -98,9 +98,6 @@ class AdaBoostClassifier:
                 print(f'Estimator Error: {error:.4f}')
                 print(f'Ensemble Error: {ensemble_error:.4f}')
                 print(f'Estimator Weight: {estimator_weight:.4f}')
-        
-        if verbose >= 0:
-            self.plot_loss()
 
     def predict(self, x: np.ndarray) -> np.ndarray:
         if len(self.forest) == 0:

src/ensembles/boosting/gradient.py
@@ -52,7 +52,7 @@ class GradientBoostedRegressionTree:
         
         self._set_impurity_function(impurity_type)
 
-    def fit(self, x_train: np.ndarray, y_train: np.ndarray, verbose: int = 2):
+    def fit(self, x_train: np.ndarray, y_train: np.ndarray, verbose: int = 1):
         if self.impurity_type is None:
             raise ValueError("The model is not compiled yet. Please call the compile method before fit.")
 
@@ -81,9 +81,6 @@ class GradientBoostedRegressionTree:
             if verbose >= 1:
                 print(f"-------------------[Iteration {i}/{self.n_estimators}]---------------------")
                 print('Error :', self.errors[-1])
-
-        if verbose >= 0:
-            self.plot_loss()
 
     def predict(self, x: np.ndarray) -> np.ndarray:
         if self.forest is None:

src/linear/base.py
@@ -41,7 +41,7 @@ class BaseLinearModel(ABC):
         
         self._validate_loss_function(self.loss)
 
-    def fit(self, x: np.ndarray, y: np.ndarray, epochs: int, batch_size: int = 1, verbose: int = 2):
+    def fit(self, x: np.ndarray, y: np.ndarray, epochs: int, batch_size: int = 1, verbose: int = 1):
         if self.loss is None:
             raise ValueError("The model is not compiled yet. Please call the compile method before fit.")
 
@@ -60,14 +60,8 @@ class BaseLinearModel(ABC):
             cost = training_strategy.train_epoch(mini_batches, self)
             self.cost.append(cost)
             
-            if verbose >= 2:
+            if verbose >= 1:
                 self._print_epoch_info(epoch, epochs, cost)
-                
-        if verbose >= 1:
-            self._plot_loss(epochs)
-            
-        if verbose >= 0:
-            print('Optimization Finished')
     
     def _initialize_parameters(self, x: np.ndarray, y: np.ndarray):
         self.weight_matrix = np.random.uniform(-1, 1, size=(y.shape[1], x.shape[1]))

src/tree/impurity/regression.py
@@ -50,7 +50,7 @@ class MeanAbsoluteError(RegressionImpurityMeasure):
         return np.mean(np.abs(actual - predicted))
     
     def derivative(self, actual: np.ndarray, predicted: float) -> float:
-        return np.where(actual - predicted < 0, -1, 1)
+        return -np.where(actual - predicted < 0, -1, 1)
 
 
 class Huber(RegressionImpurityMeasure):
@@ -62,11 +62,12 @@ class Huber(RegressionImpurityMeasure):
         is_small_error = np.abs(error) <= self.delta
         squared_loss = 0.5 * (error ** 2)
         linear_loss = self.delta * (np.abs(error) - 0.5 * self.delta)
+
         return np.mean(np.where(is_small_error, squared_loss, linear_loss))
     
     def derivative(self, actual: np.ndarray, predicted: float) -> float:
         error = actual - predicted
-        return np.where(
+        return -np.where(
             np.abs(error) <= self.delta, 
             error, 
             np.where(error < 0, -self.delta, self.delta)